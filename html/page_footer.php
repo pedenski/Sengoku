211ad85ae3b5bfd99ab92a1745afa734191263fd
pedenski <pmzildjian@yahoo.com>
2017-11-19 11:20:45 +0800

    working page chart and update chart

--- html/page_footer.php
+++ html/page_footer.php
@@ -50,9 +50,80 @@ $("span.log").click(function() {
 
 <script>
 $(document).ready(function() {
-  /* 
+ /* 
+  * INIT CHARTJS
+  */
+  $.ajax({
+    url: "html/page_graph_entries.php",
+    method: "GET",
+    data: {actyid: actyid},
+    success: function(data) 
+    {
+
+      $('#loading_icon').hide();
+      var parsed = $.parseJSON(data);
+      a = parsed.days;
+      b = parsed.arr;
+
+      console.log(b); 
+  
+      var data = {
+        labels : a,
+        datasets : [
+          { 
+            label : false,
+            data : b,
+            borderWidth : 1,
+            backgroundColor : "rgba(233, 62, 62, 0.45)",
+            borderColor : "#FF3860",
+            pointBackgroundColor : "#411515",
+            fill: 'false',
+            lineTension : 0,
+            pointRadius : 0,
+          }
+          
+        ]
+      };
+
+      var ctx = $("#line-chartcanvas");
+      
+
+      var options = {
+
+        title : {
+          display : true,
+          position : "top",
+          text : "2017 Top tags",
+          fontSize : 14,
+          fontColor : "#00C4A7"
+        },
+        legend : {
+          display : true,
+          display: false, //disable label
+          position : "bottom"
+        },
+        scales : {
+          yAxes : [{
+            ticks : {
+              min : 0
+            }
+          }]
+        }
+      };
+
+      var chart = new Chart( ctx, {
+        type : "line",
+        data : data,
+        options : options
+      });
+
+    
+    },
+  });
+   
+
+ /* 
   * AJAX POST FOR FORM SUBMISSION
-  *
   */
   $('#submit').click(function(event) {
   var formData = 
@@ -76,16 +147,88 @@ $(document).ready(function() {
     .done(function(data) {
      console.log(data); 
      $('table#logtable').append($(data)).fadeIn('slow'); //insert on table
-
+     updateChart(); //activate function 
   });
   event.preventDefault();
   });
 
- /* Chart
-  * Load ChartJS on .Ready
+function updateChart() {
+ /* 
+  * Update chart on log submission
   */
+  $.ajax({
+    url: "html/page_graph_entries.php",
+    method: "GET",
+    data: {actyid: actyid},
+    success: function(data) 
+    {
 
-});
+      $('#loading_icon').hide();
+      var parsed = $.parseJSON(data);
+      a = parsed.days;
+      b = parsed.arr;
+
+      console.log(b); 
+     
+      var data = {
+        labels : a,
+        datasets : [
+          { 
+            label : false,
+            data : b,
+            borderWidth : 1,
+            backgroundColor : "rgba(233, 62, 62, 0.45)",
+            borderColor : "#FF3860",
+            pointBackgroundColor : "#411515",
+            fill: 'false',
+            lineTension : 0,
+            pointRadius : 0,
+          }
+          
+        ]
+      };
+
+      var ctx = $("#line-chartcanvas");
+  
+      var options = {
+
+        title : {
+          display : true,
+          position : "top",
+          text : "2017 Top tags",
+          fontSize : 14,
+          fontColor : "#00C4A7"
+        },
+        legend : {
+          display : true,
+          display: false, //disable label
+          position : "bottom"
+        },
+        scales : {
+          yAxes : [{
+            ticks : {
+              min : 0
+            }
+          }]
+        }
+      };
+
+      var chart = new Chart( ctx, {
+        type : "line",
+        data : data,
+        options : options
+      });
+
+    
+    },
+
+  
+  });
+
+}
+
+
+}); //$(document).ready(function()
 </script>
 
 
